4ed27c676425e1e078079ba98f18f3b5a1abb0cf
Rishav Bhowmik <rishavbhowmik@outlook.com>
2020-09-17 12:13:41 +0530

    Added unit testing inside index.js

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const TCPServer = require('./src/server');
 
 module.exports.Request = Request;
 module.exports.TCPServer = TCPServer;
+
+
+const server = new TCPServer(8800, (socket)=>{
+
+  socket.on('say', async (data)=>{
+    console.log(`Client Says - ${data}`)
+  })
+
+  socket.on_close(()=>{
+    console.log("Client closed the connection");
+  })
+  socket.on_disconnect(()=>{
+    console.log("Client is disconnected");
+  })
+
+  console.log("client connected", socket.data_inflow_event_list)
+
+  socket.emit('say', "Hey We are Connected")
+})
+
+//parentPort.postMessage("server ready")
